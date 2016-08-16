69d7d791cf5f7d4296b793153220471c9e707402
Snapply <350624690@qq.com>
2016-08-16 11:57:47 +0800

    复制初始化

---
+++ app/src/main/java/com/example/snapply/databasetest/MainActivity.java
@@ -0,0 +1,96 @@
+package com.example.snapply.databasetest;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+
+/**
+ * Created by luweiling on 2016/8/12 0012.
+ */
+public class MainActivity extends Activity {
+
+    private MyDatabaseHelper dbHelper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.aitivity_main);
+        dbHelper = new MyDatabaseHelper(MainActivity.this,"BookStore.db",null,2);
+        Button btn = (Button)findViewById(R.id.create_database);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dbHelper.getWritableDatabase();
+            }
+        });
+        Button add = (Button)findViewById(R.id.增加数据);
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ContentValues values = new ContentValues();
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                values.put("name","The Da Vinci Code");
+                values.put("author","Dan Brown");
+                values.put("pages",454);
+                values.put("price",16.96);
+                db.insert("Book",null,values);
+                values.clear();
+                values.put("name","The Lost Symbol");
+                values.put("author","Dan Brown");
+                values.put("pages",510);
+                values.put("price",19.95);
+                db.insert("Book",null,values);
+            }
+        });
+        Button update = (Button)findViewById(R.id.update);
+        update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                ContentValues values = new ContentValues();
+                values.put("price",10.99);
+                db.update("Book",values,"name = ?",new String[] {"The Da Vinci Code"});
+            }
+        });
+        Button delete = (Button)findViewById(R.id.delete);
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                db.delete("Book","pages > ?",new String[]{"500"});
+            }
+        });
+        Button query = (Button)findViewById(R.id.query);
+        query.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                Cursor cursor = db.query("Book",null,null,null,null,null,null);
+                if (cursor.moveToFirst()) {
+                    do {
+                        String name = cursor.getString(cursor.getColumnIndex("name"));
+                        String author = cursor.getString(cursor.getColumnIndex("author"));
+                        int pages = cursor.getInt(cursor.getColumnIndex("pages"));
+                        double price = cursor.getDouble(cursor.getColumnIndex("price"));
+                        AlertDialog.Builder aleart = new AlertDialog.Builder(MainActivity.this);
+                        aleart.setTitle("Information");
+                        aleart.setMessage("Name: " + name + ";\n" +
+                                "Author: " + author + ";\n" +
+                                "Pages: " + pages + ";\n" +
+                                "Price: " + price + ".");
+                        aleart.setCancelable(false);
+                        aleart.setPositiveButton("OK",null);
+                        aleart.show();
+                    } while (cursor.moveToNext());
+                }
+            }
+        });
+    }
+}
